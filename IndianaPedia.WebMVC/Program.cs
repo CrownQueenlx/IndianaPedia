ffa5d1c849ec4b39a938b6a1abf02d53000fb0cd
Brianne W <brianne4god@gmail.com>
2023-09-18 16:57:33 -0400

    EntityFrameworkCore included, edited Program.cs, initiated AppDbContext

--- IndianaPedia.WebMVC/Program.cs
+++ IndianaPedia.WebMVC/Program.cs
@@ -1,6 +1,19 @@
+using IndianaPedia.Data;
+using IndianaPedia.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// DbContext configuration, adds the DbContext for dependency injection
+var connectionString = builder.Configuration.GetConnectionString("IndianaPedia");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddScoped<IMajorCitiesService, MajorCitiesService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
